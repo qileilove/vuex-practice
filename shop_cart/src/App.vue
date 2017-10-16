ccf2aad5bb022e5ffebd83d24433c3005e6eba01
Lei Qi <qileilove@gmail.com>
2017-10-16 15:05:51 +0800

    vuex and vue-cli for shop cart demo

---
+++ shop_cart/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>Shopping Cart Example</h1>
+    <hr>
+    <h2>Products</h2>
+    <product-list></product-list>
+    <hr>
+    <cart></cart>
+  </div>
+</template>
+
+<script>
+import ProductList from './components/ProductList.vue'
+import Cart from './components/Cart.vue'
+
+export default {
+  components: { ProductList, Cart }
+}
+</script>
